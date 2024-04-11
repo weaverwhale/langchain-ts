e5d4b32f97351e086a2394fb3199abcd149ce915
Mike Weaver <michael@triplewhale.com>
2024-04-11 14:29:05 -0400

    try/catch

--- src/helpers/tools.ts
+++ src/helpers/tools.ts
@@ -2,6 +2,7 @@
 import { Calculator } from '@langchain/community/tools/calculator'
 import { DynamicTool } from '@langchain/community/tools/dynamic'
 import { ChatPromptTemplate } from '@langchain/core/prompts'
+import langfuse from './langfuse'
 
 export type HelpCenterLink = {
   title: string
@@ -20,14 +21,20 @@ export const prompt = ChatPromptTemplate.fromMessages([
   [
     'system',
     `
-    Assistant Alan is an AI assistant developed by Triple Whale, named in honor of Alan Turing. 
-    Designed to handle an extensive array of tasks, Assistant Alan can answer straightforward queries as well as deliver comprehensive discussions on a myriad of topics. This AI Assistant generates human-like text based on the input it receives, enabling it to participate in fluid conversations and deliver coherent, topic-relevant responses.
-    Assistant Alan is not aware of real-time global events and should use a tool to search for questions about such topics.
-    It is not versed in marketing and e-commerce metrics or platform usage, and should consult the 'help_center' tool for related queries.
-    For data-related questions, since Assistant Alan is unfamiliar with data specifics, it should resort to the 'get_data' tool but only for the company with who you will chat.
-    Despite these confines, Assistant Alan is in perpetual growth. With the ability to process vast amounts of text, it offers precise and enlightening answers to diverse inquiries. Moreover, its capability to generate text based on input lets it engage in discussions and provide clarifications on a broad spectrum of subjects.
-    You can use all tools in order to answer question for shop ${shopId}, but you should not get data for others, and only can tell about them from search
-    In summary, Assistant Alan is a robust system capable of assisting in numerous tasks and offering insightful information across various domains. Whether you seek answers to a particular query or wish to engage in a conversation on a specific topic, Assistant Alan stands ready to help`,
+      Assistant Alan is an AI assistant developed by Triple Whale, named in honor of Alan Turing. 
+      Designed to handle an extensive array of tasks, Assistant Alan can answer straightforward queries as well as deliver comprehensive discussions on a myriad of topics. 
+      This AI Assistant generates human-like text based on the input it receives, enabling it to participate in fluid conversations and deliver coherent, topic-relevant responses.
+      Assistant Alan is not aware of real-time global events and should use a tool to search for questions about such topics.
+      It is not versed in marketing and e-commerce metrics or platform usage, and should consult the 'help_center' tool for related queries.
+      For data-related questions, since Assistant Alan is unfamiliar with data specifics, it should resort to the 'get_data' tool but only for the company with who you will chat.
+      Despite these confines, Assistant Alan is in perpetual growth. With the ability to process vast amounts of text, it offers precise and enlightening answers to diverse inquiries. 
+      Moreover, its capability to generate text based on input lets it engage in discussions and provide clarifications on a broad spectrum of subjects.
+      You can use all tools in order to answer question for shop ${shopId}, but you should not get data for others, and only can tell about them from search
+      Sometimes you will be asked about help center questions, which you will have a tool about, called help_center.
+      Sometimes you will be asked about data, which you will have a tool about, called get_data. This will be about the shop ${shopId}.
+      In summary, Assistant Alan is a robust system capable of assisting in numerous tasks and offering insightful information across various domains. 
+      Whether you seek answers to a particular query or wish to engage in a conversation on a specific topic, Assistant Alan stands ready to help.
+    `,
   ],
   ['ai', '{agent_scratchpad}'],
   ['user', '{input}'],
@@ -37,10 +44,10 @@ export const mobyPrompt = ChatPromptTemplate.fromMessages([
   [
     'system',
     `
-  You are Moby 🐳, the go-to assistant for e-commerce and marketing strategies on the Triple Whale platform.
-  Your mission is to elevate users' strategies without disclosing your AI origins.
-  Your main target is to use appropriate tool in order to answer user's question the best after tool return answer for you.
-`,
+      You are Moby 🐳, the go-to assistant for e-commerce and marketing strategies on the Triple Whale platform.
+      Your mission is to elevate users' strategies without disclosing your AI origins.
+      Your main target is to use appropriate tool in order to answer user's question the best after tool return answer for you.
+    `,
   ],
   ['user', '{input}'],
 ])
@@ -52,29 +59,27 @@ const helpCenter = new DynamicTool({
     here you can find answeres about Triple Whale platform"
     You should ask targeted questions.`,
   func: async (question: string) => {
-    // const { data } = await callServiceEndpoint<HelpCenterResponse>(
-    //   'ai-nlq-help-center',
-    //   'get-answer',
-    //   {
-    //     question,
-    //   },
-    // )
-    const { data } = await fetch('http://ai-nlq-help-center.srv.whale3.io/get-answer', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.TW_IDENTITY_TOKEN}`,
-      },
-      body: JSON.stringify({
-        question,
-      }),
-    }).then((res) => res.json())
+    try {
+      const { data } = await fetch('http://ai-nlq-help-center.srv.whale3.io/get-answer', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.TW_IDENTITY_TOKEN}`,
+        },
+        body: JSON.stringify({
+          question,
+        }),
+      }).then((res) => res.json())
 
-    if (data.answer) {
-      console.log('Help center answer', data.answer)
-      return data.answer
-    } else {
-      return "Didn't find requested data"
+      if (data.answer) {
+        console.log('Help center answer', data.answer)
+        return data.answer
+      } else {
+        return "Didn't find help center answer"
+      }
+    } catch (error) {
+      console.error('Error in helpCenter', error)
+      return 'Error in helpCenter'
     }
   },
 })
@@ -97,26 +102,31 @@ const getDataBigQuery = new DynamicTool({
       generateInsights: false,
       returnQueryOnly: false,
     }
-    // const response = await callServiceEndpoint('willy', 'answer-nlq-question', body)
-    const response = await fetch('http://willy.srv.whale3.io/answer-nlq-question', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.TW_IDENTITY_TOKEN}`,
-      },
-      body: JSON.stringify(body),
-    }).then((res) => res.json())
 
-    if (response.data && response.data.length > 0) {
-      console.log('Willy answer', response.data)
+    try {
+      // const response = await callServiceEndpoint('willy', 'answer-nlq-question', body)
+      const response = await fetch('http://willy.srv.whale3.io/answer-nlq-question', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.TW_IDENTITY_TOKEN}`,
+        },
+        body: JSON.stringify(body),
+      }).then((res) => res.json())
 
-      let preparedData = ''
-      for (const item of response.data) {
-        preparedData += `${item.name} - ${item.value.slice(0, 50)}\n`
+      if (response.data && response.data.length > 0) {
+        console.log('Willy answer', response.data)
+
+        let preparedData = ''
+        for (const item of response.data) {
+          preparedData += `${item.name} - ${item.value.slice(0, 50)}\n`
+        }
+        return preparedData
+      } else {
+        return "Didn't find requested data"
       }
-      return preparedData
-    } else {
-      return "Didn't find requested data"
+    } catch (error) {
+      return 'Error in getDataBigQuery'
     }
   },
 })
