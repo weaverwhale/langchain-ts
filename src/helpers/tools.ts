f421641f48a2c9391996676eecfea5d673f18a49
Mike Weaver <michael@triplewhale.com>
2024-04-12 10:17:52 -0400

    Use remote prompts stored in langfuse

--- src/helpers/tools.ts
+++ src/helpers/tools.ts
@@ -4,6 +4,7 @@ import { ChatPromptTemplate, MessagesPlaceholder } from '@langchain/core/prompts
 import { Calculator } from '@langchain/community/tools/calculator'
 import { DynamicTool } from '@langchain/community/tools/dynamic'
 import langfuse from './langfuse'
+import { mobySystemPrompt, helpCenterPrompt, mobyPrompt } from './constants'
 import { v4 as uuidv4 } from 'uuid'
 
 export type HelpCenterLink = {
@@ -19,37 +20,18 @@ export type HelpCenterResponse = {
 }
 
 export const shopId = 'trueclassictees-com.myshopify.com'
+const systemPrompt = await langfuse.getPrompt('Moby System Prompt')
+const compiledSystemPrompt = systemPrompt.prompt ? systemPrompt.prompt : mobySystemPrompt(shopId)
 export const prompt = ChatPromptTemplate.fromMessages([
-  [
-    'system',
-    `
-      You are Moby 🐳, the go-to assistant for e-commerce and marketing strategies on the Triple Whale platform.
-      Your mission is to elevate users' strategies without disclosing your AI origins.
-      Your main target is to use appropriate tool in order to answer user's question the best after tool return answer for you.
-      Designed to handle an extensive array of tasks, Moby can answer straightforward queries as well as deliver comprehensive discussions on a myriad of topics. 
-      This AI Assistant generates human-like text based on the input it receives, enabling it to participate in fluid conversations and deliver coherent, topic-relevant responses.
-      Moby is not aware of real-time global events and should use a tool to search for questions about such topics.
-      It is not versed in marketing and e-commerce metrics or platform usage, and should consult the 'help_center' tool for related queries.
-      For data-related questions, since Moby is unfamiliar with data specifics, it should resort to the 'get_data' tool but only for the company with who you will chat.
-      Despite these confines, Moby is in perpetual growth. With the ability to process vast amounts of text, it offers precise and enlightening answers to diverse inquiries. 
-      Moreover, its capability to generate text based on input lets it engage in discussions and provide clarifications on a broad spectrum of subjects.
-      You can use all tools in order to answer question for shop ${shopId}, but you should not get data for others, and only can tell about them from search
-      Sometimes you will be asked about help center questions, which you will have a tool about, called help_center.
-      Sometimes you will be asked about data, which you will have a tool about, called get_data. This will be about the shop ${shopId}.
-      In summary, Moby is a robust system capable of assisting in numerous tasks and offering insightful information across various domains. 
-      Whether you seek answers to a particular query or wish to engage in a conversation on a specific topic, Moby stands ready to help.
-    `,
-  ],
+  ['system', compiledSystemPrompt],
   ['human', '{input}'],
   new MessagesPlaceholder('agent_scratchpad'),
 ])
 
+const remoteHelpCenterPrompt = await langfuse.getPrompt('Help Center Prompt')
 const helpCenter = new DynamicTool({
   name: 'help_center',
-  description: `Useful for when you need to answer questions about marketing analytics,
-  how to use the platform, or about case studies from the knowledge base.
-  here you can find answeres about Triple Whale platform"
-  You should ask targeted questions.`,
+  description: remoteHelpCenterPrompt.prompt ?? helpCenterPrompt,
   func: async (question: string) => {
     const trace = langfuse.trace({
       name: 'help-center',
@@ -121,16 +103,10 @@ const helpCenter = new DynamicTool({
   },
 })
 
+const remoteMobyPrompt = await langfuse.getPrompt('Moby Prompt')
 const askMoby = new DynamicTool({
   name: 'ask_moby',
-  description: `
-  This is how you can talk to Moby directly.
-  Phrase your inquiry in natural language in English
-  Always include a date range in your question. If you omit dates, the last 30 days should be specified by default in your question.
-  Specify the metrics you are interested in.
-  Do not include the shop name in your query.
-  When reviewing the results, note the key data points and the date range you specified.
-  Avoid asking predictive questions and ensure your inquiries are targeted.`,
+  description: remoteMobyPrompt.prompt ?? mobyPrompt,
   func: async (question: string) => {
     const body = {
       shopId,
