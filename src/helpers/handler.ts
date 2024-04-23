b4cfc85a44cd10d2b018a337a7b3c3ba64db7065
Mike Weaver <michael@triplewhale.com>
2024-04-23 17:56:03 -0400

    Add cache mechanism for status

--- src/helpers/handler.ts
+++ src/helpers/handler.ts
@@ -2,11 +2,18 @@ import { Request, Response } from 'express'
 import { askQuestion } from './ask'
 import { getStatus } from './status'
 
+let statusCache: any = null
+let cacheTime: number = 0
+const FIVE_MINUTES = 5 * 60 * 1000
+
 export const handler = async (req: Request, res: Response, context: SourceType) => {
   const conversationId = req.body?.conversationId?.toString().trim() ?? null
   const url = req.body?.url?.trim() ?? null
   const data = req.body?.data ?? null
   let input = req.body?.question?.trim() ?? null
+
+  const isStatus = context === 'status'
+  const currentTime = Date.now()
 
   if (input?.length <= 0 && url?.length <= 0 && data?.length <= 0 && context !== 'status')
     return res.json({
@@ -22,12 +29,20 @@ export const handler = async (req: Request, res: Response, context: SourceType) 
       console.error(e)
       return res.status(500).send(e)
     }
-  } else if (context === 'status') {
-    try {
-      input = await getStatus().then((res) => JSON.stringify(res))
-    } catch (e) {
-      console.error(e)
-      return res.status(500).send(e)
+  } else if (isStatus) {
+    if (statusCache && currentTime - cacheTime < FIVE_MINUTES) {
+      return res.json({
+        answer: statusCache,
+        isCached: true,
+        cacheTime,
+      })
+    } else {
+      try {
+        input = await getStatus().then((res) => JSON.stringify(res))
+      } catch (e) {
+        console.error(e)
+        return res.status(500).send(e)
+      }
     }
   } else if (data.length > 0) {
     input = JSON.stringify(data)
@@ -35,6 +50,12 @@ export const handler = async (req: Request, res: Response, context: SourceType) 
 
   try {
     const answer = await askQuestion(input, context, conversationId)
+
+    if (isStatus && answer) {
+      statusCache = answer
+      cacheTime = currentTime
+    }
+
     return res.json({
       answer: answer ?? 42,
     })
